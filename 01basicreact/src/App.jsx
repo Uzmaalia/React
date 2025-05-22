073b7418ed42c3a3971b5265e5cc30121834b0c7
Uzmaalia <uzmaaalia1416@gmail.com>
2025-05-22 20:26:38 +0530

    beginning

---
+++ 01basicreact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from './Chai'
+function App() {
+  
+  return (
+    //fragment
+   <>  
+   <Chai/>
+   <h1>chai aur code</h1>
+   <p>test para</p>
+   </>
+  )
+}
+
+export default App

0dfe2eb353c3ec1d06a477a594820c0cb42dce9e
Uzmaalia <uzmaaalia1416@gmail.com>
2025-05-24 12:39:28 +0530

    custom react

--- 01basicreact/src/App.jsx
+++ 01basicreact/src/App.jsx
@@ -1,11 +1,13 @@
 import Chai from './Chai'
+
 function App() {
+  const username = "jupiter"
   
   return (
     //fragment
    <>  
    <Chai/>
-   <h1>chai aur code</h1>
+   <h1>chai aur code {username}</h1>
    <p>test para</p>
    </>
   )
--- 01basicreact/src/main.jsx
+++ 01basicreact/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
@@ -10,9 +10,33 @@ function MyApp(){
   )
 }
 
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>visit google</a>
+)
+
+const anotherUser = 'chai aur react'
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click me to visit google',
+  anotherUser
+)
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <MyApp />
-    {/* MyApp() */}
-  </StrictMode>,
+  
+    // reactElement
+    reactElement
+
 )

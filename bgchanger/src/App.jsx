470a75e85ff02167aed81f26038b0311088d9785
Uzmaalia <uzmaaalia1416@gmail.com>
2025-05-27 19:34:05 +0530

    background colour change

---
+++ bgchanger/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState('pink')
+
+  return (
+    <div className="w-full h-screen duration-200"
+    style={{backgroundColor: color}}
+    
+    >
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        test
+
+      </div>
+    
+
+    </div>
+  )
+}
+
+export default App

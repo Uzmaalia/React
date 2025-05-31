3170bfb13456f2e790fa16a594617fe08f8b85aa
Uzmaalia <uzmaaalia1416@gmail.com>
2025-05-31 12:08:07 +0530

    background color changer

--- bgchanger/src/App.jsx
+++ bgchanger/src/App.jsx
@@ -12,7 +12,38 @@ function App() {
     
     >
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
-        test
+
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button 
+          className='outline-none px-4 py-1 rounded-full text-white'
+          style={{backgroundColor: "red"}}
+          onClick={() => setColor('red')}
+          >Red</button>
+        </div>
+
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button 
+          onClick={() => setColor('green')}
+          className='outline-none px-4 py-1 rounded-full text-white'
+          style={{backgroundColor: "green"}}
+          >Green</button>
+        </div>
+
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button 
+          onClick={() => setColor('blue')}
+          className='outline-none px-4 py-1 rounded-full text-white'
+          style={{backgroundColor: "blue"}}
+          >Blue</button>
+        </div>
+
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button 
+          onClick={() => setColor('orange')}
+          className='outline-none px-4 py-1 rounded-full text-white'
+          style={{backgroundColor: "orange"}}
+          >Orange</button>
+        </div>
 
       </div>
     
